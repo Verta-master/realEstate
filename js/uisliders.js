1dcad88bbc5546c544e4a5c3bdf4b4c1c71d17f5
Verta-master <vertyagina@gmail.com>
2017-12-02 20:53:01 +0600

    Update 2017-12-02T14:52:48.186Z

--- js/uisliders.js
+++ js/uisliders.js
@@ -1,14 +1,30 @@
-// Range slider
+// Range sliders
 $( function() {
   $( "#slider-range" ).slider({
     range: true,
-    min: 0,
-    max: 500,
-    values: [ 75, 300 ],
+    min: 50000,
+    max: 8000000,
+    values: [ 50000, 8000000 ],
     slide: function( event, ui ) {
-      $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+      $( "#amount-min" ).val( ui.values[ 0 ] );
+      $( "#amount-max" ).val( ui.values[ 1 ] );
     }
   });
-  $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-    " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+  $( "#amount-min" ).val( $( "#slider-range" ).slider( "values", 0 ) );
+  $( "#amount-max" ).val( $( "#slider-range" ).slider( "values", 1 ) );
+});
+
+$( function() {
+  $( "#slider-range2" ).slider({
+    range: true,
+    min: 30,
+    max: 94,
+    values: [ 30, 94 ],
+    slide: function( event, ui ) {
+      $( "#amount-min2" ).val( ui.values[ 0 ] + " м2" );
+      $( "#amount-max2" ).val( ui.values[ 1 ] + " м2" );
+    }
+  });
+  $( "#amount-min2" ).val( $( "#slider-range2" ).slider( "values", 0 ) + " м2" );
+  $( "#amount-max2" ).val( $( "#slider-range2" ).slider( "values", 1 ) + " м2" );
 });
